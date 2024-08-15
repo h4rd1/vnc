6a89948c4b3acf07da50cb64690ed367636e34c0
Александр <43719873+h4rd1@users.noreply.github.com>
2024-08-15 21:54:29 +0300

    Delete xtsartap.sh

--- xtsartap.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-[ -x /etc/vnc/xstartap ] && exec /etc/vnc/xstartup
-[ -r $HOME/.Xresources ] && xrdb $HOME/.Xresources
-vncconfig -iconic &
-dbus-launch --exit-with-session gnome-session &
